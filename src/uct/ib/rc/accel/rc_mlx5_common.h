1fee5bfc3f6b212cbb7c211ca0d93ee8bf4b6665
Artem Polyakov <artpol84@gmail.com>
2017-09-14 15:32:44 +0700

    rc_mlx5: Prefetch the very first buffers header to reduce the latency
    Conflicts:
    	src/uct/ib/rc/accel/rc_mlx5_common.h

--- src/uct/ib/rc/accel/rc_mlx5_common.h
+++ src/uct/ib/rc/accel/rc_mlx5_common.h
@@ -24,7 +24,6 @@
 #define UCT_RC_MLX5_CHECK_AM_SHORT(_id, _length, _av_size) \
     UCT_RC_CHECK_AM_SHORT(_id, _length, UCT_IB_MLX5_AM_MAX_SHORT(_av_size))
 
-
 /* there is no need to do a special check for length == 0 because in that
  * case wqe size is valid: inl + raddr + dgram + ctrl fit in 2 WQ BB
  */
@@ -50,6 +49,17 @@ typedef struct uct_rc_mlx5_iface_common {
     UCS_STATS_NODE_DECLARE(stats);
 } uct_rc_mlx5_iface_common_t;
 
+
+#define UCT_RC_MLX5_SRQ_PREFETCH_FIRST(iface)                         \
+{                                                                           \
+    unsigned wqe_ctr = (iface->rx.srq.free_idx + 1) & iface->rx.srq.mask;   \
+    uct_ib_mlx5_srq_seg_t *seg;                                             \
+    void *ptr;                                                              \
+    seg = uct_ib_mlx5_srq_get_wqe(&iface->rx.srq, wqe_ctr);                 \
+    uct_ib_iface_recv_desc_t *desc = seg->srq.desc;                         \
+    ptr = (void*)uct_ib_iface_recv_desc_hdr(&rc_iface->super, desc);        \
+    ucs_prefetch(ptr);                                                      \
+}
 
 unsigned uct_rc_mlx5_iface_srq_post_recv(uct_rc_iface_t *iface, uct_ib_mlx5_srq_t *srq);
 
@@ -118,6 +128,8 @@ uct_rc_mlx5_iface_common_poll_rx(uct_rc_mlx5_iface_common_t *mlx5_common_iface,
 
     ucs_assert(uct_ib_mlx5_srq_get_wqe(&mlx5_common_iface->rx.srq,
                                        mlx5_common_iface->rx.srq.mask)->srq.next_wqe_index == 0);
+
+    UCT_RC_MLX5_SRQ_PREFETCH_FIRST(mlx5_common_iface);
 
     cqe = uct_ib_mlx5_poll_cq(&rc_iface->super, &mlx5_common_iface->rx.cq);
     if (cqe == NULL) {
