8709538933829ff77cc02a40161cf7197cb37e8a
Artem Polyakov <artemp@mellanox.com>
2017-10-07 20:43:35 +0300

    rc_mlx5: add the flag to poll_rx to distinguish between rc and dc

--- src/uct/ib/rc/accel/rc_mlx5_common.h
+++ src/uct/ib/rc/accel/rc_mlx5_common.h
@@ -110,7 +110,7 @@ uct_rc_mlx5_iface_common_rx_inline(uct_rc_mlx5_iface_common_t *mlx5_iface,
 
 static UCS_F_ALWAYS_INLINE unsigned
 uct_rc_mlx5_iface_common_poll_rx(uct_rc_mlx5_iface_common_t *mlx5_common_iface,
-                                 uct_rc_iface_t *rc_iface)
+                                 uct_rc_iface_t *rc_iface, char have_scat32)
 {
     uct_ib_mlx5_srq_seg_t *seg;
     uct_ib_iface_recv_desc_t *desc;
@@ -147,7 +147,7 @@ uct_rc_mlx5_iface_common_poll_rx(uct_rc_mlx5_iface_common_t *mlx5_common_iface,
     /* Get a pointer to AM header (after which comes the payload)
      * Support cases of inline scatter by pointing directly to CQE.
      */
-    if (cqe->op_own & MLX5_INLINE_SCATTER_32) {
+    if (have_scat32 && cqe->op_own & MLX5_INLINE_SCATTER_32) {
         hdr = (uct_rc_hdr_t*)(cqe);
         uct_rc_mlx5_iface_common_rx_inline(mlx5_common_iface, rc_iface, desc,
                                            UCT_RC_MLX5_IFACE_STAT_RX_INL_32, byte_len);
