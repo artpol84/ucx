dfde59b68c5162afb05bb99b2562cf32e5c6f6fa
Artem Polyakov <artpol84@gmail.com>
2019-08-03 17:03:16 -0700

    debug

--- src/ucp/core/ucp_worker.c
+++ src/ucp/core/ucp_worker.c
@@ -102,6 +102,9 @@ void lock_profile_spinlock(ucs_spinlock_t *lock)
     locking_profile_t *prof = ucx_lock_dbg_thread_local();
     pthread_t self = pthread_self();
 
+    printf("Self = %p, owner=%p, lock = %u, count = %u",
+           (void*)self, (void*)lock->owner, lock->lock, lock->count);
+
     if (ucs_spin_is_owner(lock, self)) {
         ++lock->count;
         return;
